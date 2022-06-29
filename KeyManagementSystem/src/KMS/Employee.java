03fa6e6cbf336933355626cc0da45d30bfc1e154
Edward Hinds <hindsedward@gmail.com>
2022-06-29 13:52:29 -0400

    Initial Project Setup / Employee Class
    Initial project set up and the initial version of the Employee Class. Need to create Unit tests and pass them before I push the tasks to complete on projects page. Tried setting this on a new branch but wasn't able to. I think this is due to the repository being empty.

---
+++ KeyManagementSystem/src/KMS/Employee.java
@@ -0,0 +1,27 @@
+package KMS;
+
+public class Employee {
+	
+	String name;
+	int id;
+	
+	//constructor 
+	public Employee(String name, int id){
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
