e0fb9b8fa2ca55328666cbb283d768ea13254be1
Edward Hinds <hindsedward@gmail.com>
2022-07-03 07:52:57 -0400

    Changed Employee constructor method
    The constructor method has been updated to pass the unit tests.

--- KeyManagementSystem/src/kms/Employee.java
+++ KeyManagementSystem/src/kms/Employee.java
@@ -7,6 +7,10 @@ public class Employee {
 	
 	//constructor 
 	public Employee(String name, int id){
+		if(name.length()==0)
+			throw new RuntimeException("Name must have length >0");
+		if(id < 1000 || id > 9999)
+			throw new RuntimeException("ID must be inbetween 1000-9999");
 		this.name = name;
 		this.id = id;
 	}
