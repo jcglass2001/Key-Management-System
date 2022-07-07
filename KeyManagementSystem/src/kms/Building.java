a903b75992ebfe92986bcf9bef3de4f115e5268a
Ethan Shufflebarger <90651731+ethanshuffle@users.noreply.github.com>
2022-07-06 22:21:49 -0400

    got rid of unecessary array

--- KeyManagementSystem/src/kms/Building.java
+++ KeyManagementSystem/src/kms/Building.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 
 public class Building {
 	
-	//We will load all buildings into this ArrayList from the text
-	//file and add any new ones to it. We will also write this into
-	//the text file when saving.
-	private ArrayList<Building> buildings = new ArrayList<>();
 	private String name;
 	private String code;
 	
