667507691ab29c1e125d38ea1a818e1b0414af92
Edward Hinds <hindsedward@gmail.com>
2022-07-10 18:30:55 -0400

    Created CompanyTest
    Created a test class to write unit tests for Company Class.

---
+++ KeyManagementSystem/src/kms/CompanyTest.java
@@ -0,0 +1,14 @@
+package kms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompanyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
