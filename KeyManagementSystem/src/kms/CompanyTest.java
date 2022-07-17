209d446b872350eeb24d75f69c9848eddc013c9a
Edward Hinds <hindsedward@gmail.com>
2022-07-17 08:54:04 -0400

    Add more test cases
    Added test cases to test remove functionality of building, suites, rooms, and employees as per documentation requirements.

--- KeyManagementSystem/src/kms/CompanyTest.java
+++ KeyManagementSystem/src/kms/CompanyTest.java
@@ -19,6 +19,19 @@ class CompanyTest {
 
 	}
 	
+	@DisplayName("removes buildings")
+	@Test
+	void RemoveBuildings() {
+		Company c1 = new Company();
+		Building b1 = new Building("Google", "01");
+		Building b2 = new Building("Microsoft", "02");
+		c1.getBuildings().add(b1);
+		c1.getBuildings().add(b2);
+		c1.getBuildings().remove(b1);
+		assertEquals(c1.getBuildings().get(0).getName(), "Microsoft");
+
+	}
+	
 	@DisplayName("saves suites")
 	@Test
 	void AddSuites() {
@@ -26,6 +39,18 @@ class CompanyTest {
 		Suite s1 = new Suite("Offices", "12", "01");
 		c1.getSuites().add(s1);
 		assertEquals(c1.getSuites().get(0).getName(), "Offices");
+	}
+	
+	@DisplayName("removes suites")
+	@Test
+	void RemovesSuites() {
+		Company c1 = new Company();
+		Suite s1 = new Suite("Offices", "12", "01");
+		Suite s2 = new Suite("Lounge", "12", "02");
+		c1.getSuites().add(s1);
+		c1.getSuites().add(s2);
+		c1.getSuites().remove(s1);
+		assertEquals(c1.getSuites().get(0).getName(), "Lounge");
 	}
 	
 	@DisplayName("saves rooms")
@@ -37,6 +62,18 @@ class CompanyTest {
 		assertEquals(c1.getRooms().get(0).getRoomNumber(), "432");
 	}
 	
+	@DisplayName("removes rooms")
+	@Test
+	void RemoveRooms() {
+		Company c1 = new Company();
+		Room r1 = new Room("01", "12", "432");
+		Room r2 = new Room("01", "12", "433");
+		c1.getRooms().add(r1);
+		c1.getRooms().add(r2);
+		c1.getRooms().remove(r1);
+		assertEquals(c1.getRooms().get(0).getRoomNumber(), "433");
+	}
+	
 	@DisplayName("saves employees")
 	@Test
 	void AddEmployees() {
@@ -44,6 +81,18 @@ class CompanyTest {
 		Employee e1 = new Employee("Joseph", "1234");
 		c1.getEmployees().add(e1);
 		assertEquals(c1.getEmployees().get(0).getName(), "Joseph");
+	}
+	
+	@DisplayName("removes employees")
+	@Test
+	void RemoveEmployees() {
+		Company c1 = new Company();
+		Employee e1 = new Employee("Joseph", "1234");
+		Employee e2 = new Employee("John", "4321");
+		c1.getEmployees().add(e1);
+		c1.getEmployees().add(e2);
+		c1.getEmployees().remove(e1);
+		assertEquals(c1.getEmployees().get(0).getName(), "John");
 	}
 	
 	@DisplayName("FileWriter: create file")
@@ -58,7 +107,7 @@ class CompanyTest {
 		c1.getSuites().add(s1);
 		c1.getRooms().add(r1);
 		c1.getEmployees().add(e1);
-		Company.WriteToFile();
+		c1.writeToFile();
 		File testFile = new File("Report.txt");
 		assertEquals(testFile.exists(), true);
 	}
